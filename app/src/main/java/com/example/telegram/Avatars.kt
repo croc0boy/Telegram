0af4cb64e64e9532f0b4d204003441dead85d599
croc0boy <sunat.rustamov1@gmail.com>
2022-05-16 18:23:25 +0500

    fixed problems with glide

--- app/src/main/java/com/example/telegram/Avatars.kt
+++ app/src/main/java/com/example/telegram/Avatars.kt
@@ -4,72 +4,72 @@ val accounts = arrayListOf(
     Account(
         id = 0,
         name = "Azamat Sultanov",
-        image = "https://sdelaicomp.ru/wp-content/uploads/2019/06/AnimeNaAvuVVK_krut5.jpg",
+        image = "https://expresssmart.ru/wp-content/uploads/kirito-sword-art-online-31698905-1280-720.jpg",
         lastMessage = "Там вообще другая история",
         messageTime = "1:23"
     ),
     Account(
         id = 1,
         name = "Naimov Fayyoz",
-        image = "https://sdelaicomp.ru/wp-content/uploads/2019/06/AnimeNaAvuVVK_krut5.jpg",
+        image = "https://pikuco.ru/upload/test_stable/eca/eca08365ec9c9646521e27f51f95bdbf.jpg",
         lastMessage = "бля я пасспорт в саме забыл",
         messageTime = "18:01"
     ),
     Account(
         id = 2,
         name = "Azim Salimov",
-        image = "https://sdelaicomp.ru/wp-content/uploads/2019/06/AnimeNaAvuVVK_krut5.jpg",
+        image = "https://steamblog.ru/wp-content/uploads/2021/05/cs-go-niki-5.jpg",
         lastMessage = "Можешь ему скинуть ссылку он у меня не выходит почему то",
         messageTime = "10:43"
     ),Account(
         id = 3,
         name = "Rustamov Sunatillo",
-        image = "https://sdelaicomp.ru/wp-content/uploads/2019/06/AnimeNaAvuVVK_krut5.jpg",
+        image = "https://nntheblog.b-cdn.net/wp-content/uploads/2021/11/E0zFM94WYAofgid-edited.jpg",
         lastMessage = "нужно было найти твою авку для ревью листа)",
         messageTime = "12:25"
     ),Account(
         id = 4,
         name = "Feruz Xodjaev",
-        image = "https://sdelaicomp.ru/wp-content/uploads/2019/06/AnimeNaAvuVVK_krut5.jpg",
+        image = "https://shikimori.one/system/characters/original/113609.jpg",
         lastMessage = "Ну иди спать значит, я тоже тут уже карты путаю",
         messageTime = "19:01"
     ),Account(
         id = 5,
         name = "Aziz Ulmasov",
-        image = "https://sdelaicomp.ru/wp-content/uploads/2019/06/AnimeNaAvuVVK_krut5.jpg",
+        image = "https://e8vuu6mw6zt.exactdn.com/wp-content/uploads/2021/09/00-featured-senkuu-dr-stone-anime.jpg",
         lastMessage = "учеба закончилась чтоль?",
         messageTime = "1:23"
     ),
     Account(
         id = 6,
         name = "Anya Pak",
-        image = "https://sdelaicomp.ru/wp-content/uploads/2019/06/AnimeNaAvuVVK_krut5.jpg",
+        image = "https://irecommend.ru/sites/default/files/imagecache/copyright1/user-images/431460/evyPkLS1ZWqCtn2Fzqos2A.jpg",
         lastMessage = "Ты же на рекурсию вьетнамские флешбеки ловишь",
         messageTime = "10:23"
     )
     ,Account(
         id = 7,
         name = "Nazar",
-        image = "https://sdelaicomp.ru/wp-content/uploads/2019/06/AnimeNaAvuVVK_krut5.jpg",
+        image = "https://avatars.mds.yandex.net/get-zen_doc/1925657/pub_5dfcbe0106cc4600ad9b8d77_5dfcc18898930900afd4ce42/scale_1200",
         lastMessage = "А как мне это лучше сделать",
         messageTime = "8:03"
     )
     ,Account(
         id = 8,
         name = "David Alkyaman",
-        image = "https://sdelaicomp.ru/wp-content/uploads/2019/06/AnimeNaAvuVVK_krut5.jpg",
+        image = "https://vev.ru/wp-content/uploads/2021/12/berserk_228-1.jpg",
         lastMessage = "Мб кто-нибудь из нас утром сгоняет за ними?",
         messageTime = "7:30"
     ),Account(
         id = 9,
         name = "Shoxrux Kamarov",
-        image = "https://sdelaicomp.ru/wp-content/uploads/2019/06/AnimeNaAvuVVK_krut5.jpg",
+        image = "https://e8vuu6mw6zt.exactdn.com/wp-content/uploads/2021/08/00-featured-killua-zoldyck-hunter-x-hunter-anime-preview.jpg",
         lastMessage = "Скинь фото пасспорта",
         messageTime = "11:47"
     ),Account(
         id = 10,
         name = "Murod Murodov",
-        image = "https://sdelaicomp.ru/wp-content/uploads/2019/06/AnimeNaAvuVVK_krut5.jpg",
+        image = "https://e8vuu6mw6zt.exactdn.com/wp-content/uploads/2021/09/00-featured-zenitsu-agatsuma-demon-slayer-screenshot.jpg",
         lastMessage = "Ебан бля",
         messageTime = "13:10"
     )
